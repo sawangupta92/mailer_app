8f784ade7306606014c941e11ef530967411aa0b
sawan <sawan.gupta92@gmail.com>
2014-10-08 18:11:58 +0530

    remove unecessary migration files

--- db/migrate/20141001104937_change_column_type_from_attachment.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTypeFromAttachment < ActiveRecord::Migration
-  def change
-    change_column :attachments, :type, :string
-  end
-end
--- db/migrate/20141001115857_create_create_table_email_mailboxes.rb
+++
@@ -1,9 +0,0 @@
-class CreateCreateTableEmailMailboxes < ActiveRecord::Migration
-  def change
-    create_table :emails_mailboxes do |t|
-      t.belongs_to :email
-      t.belongs_to :mailbox
-      t.timestamps
-    end
-  end
-end
